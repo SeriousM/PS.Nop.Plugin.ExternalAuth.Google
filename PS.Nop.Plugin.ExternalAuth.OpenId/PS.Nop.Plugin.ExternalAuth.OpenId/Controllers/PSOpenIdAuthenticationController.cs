f708589246b84bb881ca208cada13072d34f3f2c
Bernhard Millauer <bernhard.millauer@adventics.de>
2019-02-19 14:27:18 +0100

    Add reading of firstname and lastmane

--- PS.Nop.Plugin.ExternalAuth.OpenId/PS.Nop.Plugin.ExternalAuth.OpenId/Controllers/PSOpenIdAuthenticationController.cs
+++ PS.Nop.Plugin.ExternalAuth.OpenId/PS.Nop.Plugin.ExternalAuth.OpenId/Controllers/PSOpenIdAuthenticationController.cs
@@ -18,115 +18,125 @@ using PS.Nop.Plugin.ExternalAuth.OpenId.Models;
 
 namespace PS.Nop.Plugin.ExternalAuth.OpenId.Controllers
 {
-    public class PSOpenIdAuthenticationController : BasePluginController
+  public class PSOpenIdAuthenticationController : BasePluginController
+  {
+    private readonly OpenIdExternalAuthSettings _openIdExternalAuthSettings;
+    private readonly IExternalAuthenticationService _externalAuthenticationService;
+    private readonly ILocalizationService _localizationService;
+    private readonly IPermissionService _permissionService;
+    private readonly ISettingService _settingService;
+    private readonly IGenericAttributeService _genericAttributeService;
+
+    public PSOpenIdAuthenticationController(OpenIdExternalAuthSettings openIdExternalAuthSettings,
+        IExternalAuthenticationService externalAuthenticationService,
+        ILocalizationService localizationService,
+        IPermissionService permissionService,
+        ISettingService settingService,
+        IGenericAttributeService genericAttributeService)
     {
-      private readonly OpenIdExternalAuthSettings _openIdExternalAuthSettings;
-        private readonly IExternalAuthenticationService _externalAuthenticationService;
-        private readonly ILocalizationService _localizationService;
-        private readonly IPermissionService _permissionService;
-        private readonly ISettingService _settingService;
-        private readonly IGenericAttributeService _genericAttributeService;
-
-        public PSOpenIdAuthenticationController(OpenIdExternalAuthSettings openIdExternalAuthSettings,
-            IExternalAuthenticationService externalAuthenticationService,
-            ILocalizationService localizationService,
-            IPermissionService permissionService,
-            ISettingService settingService,
-            IGenericAttributeService genericAttributeService)
-        {
-            this._openIdExternalAuthSettings = openIdExternalAuthSettings;
-            this._externalAuthenticationService = externalAuthenticationService;
-            this._localizationService = localizationService;
-            this._permissionService = permissionService;
-            this._settingService = settingService;
-            _genericAttributeService = genericAttributeService;
-        }
-
-        [AuthorizeAdmin]
-        [Area(AreaNames.Admin)]
-        public IActionResult Configure()
-        {
-            if (!_permissionService.Authorize(StandardPermissionProvider.ManageExternalAuthenticationMethods))
-                return AccessDeniedView();
-
-            var model = new ConfigurationModel
-            {
-                ClientId = _openIdExternalAuthSettings.ClientId,
-                ClientSecret = _openIdExternalAuthSettings.ClientSecret,
-                ResponseType = _openIdExternalAuthSettings.ResponseType,
-                Scopes = _openIdExternalAuthSettings.Scopes,
-                Authority = _openIdExternalAuthSettings.Authority,
-                RequiresHttps = _openIdExternalAuthSettings.RequiresHttps
-            };
-
-            return View("~/Plugins/PS.ExternalAuth.OpenId/Views/Configure.cshtml", model);
-        }
-
-        [HttpPost]
-        [AdminAntiForgery]
-        [AuthorizeAdmin]
-        [Area(AreaNames.Admin)]
-        public IActionResult Configure(ConfigurationModel model)
-        {
-            if (!_permissionService.Authorize(StandardPermissionProvider.ManageExternalAuthenticationMethods))
-                return AccessDeniedView();
-
-            if (!ModelState.IsValid)
-                return Configure();
-
-            //save settings
-            _openIdExternalAuthSettings.ClientId = model.ClientId?.Trim();
-            _openIdExternalAuthSettings.ClientSecret = model.ClientSecret?.Trim();
-            _openIdExternalAuthSettings.ResponseType = model.ResponseType?.Trim();
-            _openIdExternalAuthSettings.Scopes = model.Scopes?.Trim();
-            _openIdExternalAuthSettings.Authority = model.Authority?.Trim();
-            _openIdExternalAuthSettings.RequiresHttps = model.RequiresHttps;
-            _settingService.SaveSetting(_openIdExternalAuthSettings);
-            SuccessNotification(_localizationService.GetResource("Admin.Plugins.Saved"));
-
-            return Configure();
-        }
-
-        public IActionResult Login(string returnUrl)
-        {
-            if (!_externalAuthenticationService.ExternalAuthenticationMethodIsAvailable(OpenIdExternalAuthConstants.ProviderSystemName))
-                throw new NopException("OpenId authentication module cannot be loaded");
-
-            if (!_openIdExternalAuthSettings.IsValid())
-                throw new NopException("OpenId authentication module not configured");
-
-            //configure login callback action
-            var authenticationProperties = new AuthenticationProperties
-            {
-                RedirectUri = Url.Action("LoginCallback", "PSOpenIdAuthentication", new { returnUrl = returnUrl })
-            };
-
-            return Challenge(authenticationProperties, OpenIdConnectDefaults.AuthenticationScheme);
-        }
-
-        public async Task<IActionResult> LoginCallback(string returnUrl)
-        {
-            //authenticate Facebook user
-            var authenticateResult =  await this.HttpContext.AuthenticateAsync(OpenIdConnectDefaults.AuthenticationScheme);
-            if (!authenticateResult.Succeeded || !authenticateResult.Principal.Claims.Any())
-                return RedirectToRoute("Login");
-
-            //create external authentication parameters
-            var authenticationParameters = new ExternalAuthenticationParameters
-            {
-                ProviderSystemName = OpenIdExternalAuthConstants.ProviderSystemName,
-                Email = authenticateResult.Principal.FindFirst(claim => claim.Type == ClaimTypes.Email)?.Value,
-                ExternalIdentifier = authenticateResult.Principal.FindFirst(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value,
-                ExternalDisplayIdentifier = authenticateResult.Principal.FindFirst(claim => claim.Type == ClaimTypes.Name)?.Value,
-                Claims = authenticateResult.Principal.Claims.Select(claim => new ExternalAuthenticationClaim(claim.Type, claim.Value)).ToList()
-            };
-
-            var customer = _externalAuthenticationService.GetUserByExternalAuthenticationParameters(authenticationParameters);
-
-            _genericAttributeService.SaveAttribute(customer, NopCustomerDefaults.LastNameAttribute, "aasdasda");
-
-            //authenticate Nop user
-            return _externalAuthenticationService.Authenticate(authenticationParameters, returnUrl);
-        }
+      _openIdExternalAuthSettings = openIdExternalAuthSettings;
+      _externalAuthenticationService = externalAuthenticationService;
+      _localizationService = localizationService;
+      _permissionService = permissionService;
+      _settingService = settingService;
+      _genericAttributeService = genericAttributeService;
     }
+
+    [AuthorizeAdmin]
+    [Area(AreaNames.Admin)]
+    public IActionResult Configure()
+    {
+      if (!_permissionService.Authorize(StandardPermissionProvider.ManageExternalAuthenticationMethods))
+        return AccessDeniedView();
+
+      var model = new ConfigurationModel
+      {
+        ClientId = _openIdExternalAuthSettings.ClientId,
+        ClientSecret = _openIdExternalAuthSettings.ClientSecret,
+        ResponseType = _openIdExternalAuthSettings.ResponseType,
+        Scopes = _openIdExternalAuthSettings.Scopes,
+        Authority = _openIdExternalAuthSettings.Authority,
+        RequiresHttps = _openIdExternalAuthSettings.RequiresHttps
+      };
+
+      return View("~/Plugins/PS.ExternalAuth.OpenId/Views/Configure.cshtml", model);
+    }
+
+    [HttpPost]
+    [AdminAntiForgery]
+    [AuthorizeAdmin]
+    [Area(AreaNames.Admin)]
+    public IActionResult Configure(ConfigurationModel model)
+    {
+      if (!_permissionService.Authorize(StandardPermissionProvider.ManageExternalAuthenticationMethods))
+        return AccessDeniedView();
+
+      if (!ModelState.IsValid)
+        return Configure();
+
+      //save settings
+      _openIdExternalAuthSettings.ClientId = model.ClientId?.Trim();
+      _openIdExternalAuthSettings.ClientSecret = model.ClientSecret?.Trim();
+      _openIdExternalAuthSettings.ResponseType = model.ResponseType?.Trim();
+      _openIdExternalAuthSettings.Scopes = model.Scopes?.Trim();
+      _openIdExternalAuthSettings.Authority = model.Authority?.Trim();
+      _openIdExternalAuthSettings.RequiresHttps = model.RequiresHttps;
+      _settingService.SaveSetting(_openIdExternalAuthSettings);
+      SuccessNotification(_localizationService.GetResource("Admin.Plugins.Saved"));
+
+      return Configure();
+    }
+
+    public IActionResult Login(string returnUrl)
+    {
+      if (!_externalAuthenticationService.ExternalAuthenticationMethodIsAvailable(OpenIdExternalAuthConstants.ProviderSystemName))
+        throw new NopException("OpenId authentication module cannot be loaded");
+
+      if (!_openIdExternalAuthSettings.IsValid())
+        throw new NopException("OpenId authentication module not configured");
+
+      //configure login callback action
+      var authenticationProperties = new AuthenticationProperties
+      {
+        RedirectUri = Url.Action("LoginCallback", "PSOpenIdAuthentication", new { returnUrl = returnUrl })
+      };
+
+      return Challenge(authenticationProperties, OpenIdConnectDefaults.AuthenticationScheme);
+    }
+
+    public async Task<IActionResult> LoginCallback(string returnUrl)
+    {
+      // note: here is some information how to introduce new claims
+      // https://leastprivilege.com/2017/11/15/missing-claims-in-the-asp-net-core-2-openid-connect-handler/
+
+      var authenticateResult = await HttpContext.AuthenticateAsync(OpenIdConnectDefaults.AuthenticationScheme);
+      if (!authenticateResult.Succeeded || !authenticateResult.Principal.Claims.Any())
+        return RedirectToRoute("Login");
+
+      //create external authentication parameters
+      var authenticationParameters = new ExternalAuthenticationParameters
+      {
+        ProviderSystemName = OpenIdExternalAuthConstants.ProviderSystemName,
+        Email = authenticateResult.Principal.FindFirst(claim => claim.Type == ClaimTypes.Email)?.Value,
+        ExternalIdentifier = authenticateResult.Principal.FindFirst(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value,
+        ExternalDisplayIdentifier = authenticateResult.Principal.FindFirst(claim => claim.Type == ClaimTypes.Name)?.Value,
+        Claims = authenticateResult.Principal.Claims.Select(claim => new ExternalAuthenticationClaim(claim.Type, claim.Value)).ToList()
+      };
+
+      var customer = _externalAuthenticationService.GetUserByExternalAuthenticationParameters(authenticationParameters);
+
+      ////var idToken = authenticateResult.Properties.GetTokenValue("id_token");
+
+      var givenName = authenticateResult.Principal.FindFirst(ClaimTypes.GivenName)?.Value;
+      var familyName = authenticateResult.Principal.FindFirst(ClaimTypes.Surname)?.Value;
+
+      if (givenName != null)
+        _genericAttributeService.SaveAttribute(customer, NopCustomerDefaults.FirstNameAttribute, givenName);
+      if (familyName != null)
+        _genericAttributeService.SaveAttribute(customer, NopCustomerDefaults.LastNameAttribute, familyName);
+
+      //authenticate Nop user
+      return _externalAuthenticationService.Authenticate(authenticationParameters, returnUrl);
+    }
+  }
 }
--- PS.Nop.Plugin.ExternalAuth.OpenId/PS.Nop.Plugin.ExternalAuth.OpenId/Infrastructure/OpenIdAuthenticationRegistrar.cs
+++ PS.Nop.Plugin.ExternalAuth.OpenId/PS.Nop.Plugin.ExternalAuth.OpenId/Infrastructure/OpenIdAuthenticationRegistrar.cs
@@ -6,28 +6,31 @@ using Nop.Services.Authentication.External;
 
 namespace PS.Nop.Plugin.ExternalAuth.OpenId.Infrastructure
 {
-    public class OpenIdAuthenticationRegistrar : IExternalAuthenticationRegistrar
+  public class OpenIdAuthenticationRegistrar : IExternalAuthenticationRegistrar
+  {
+    public void Configure(AuthenticationBuilder builder)
     {
-        public void Configure(AuthenticationBuilder builder)
+      builder.AddOpenIdConnect(options =>
+      {
+        var settings = EngineContext.Current.Resolve<OpenIdExternalAuthSettings>();
+        options.Authority = settings.Authority;
+        options.ResponseType = settings.ResponseType;
+
+        options.RequireHttpsMetadata = settings.RequiresHttps;
+
+        var scopes = settings.Scopes?.Split(' ', StringSplitOptions.RemoveEmptyEntries) ?? new string[0];
+        foreach (var scope in scopes)
         {
-            builder.AddOpenIdConnect(options =>
-            {
-                var settings = EngineContext.Current.Resolve<OpenIdExternalAuthSettings>();
-                options.Authority = settings.Authority;
-                options.ResponseType = settings.ResponseType;
-
-                options.RequireHttpsMetadata = settings.RequiresHttps;
-
-                var scopes = settings.Scopes?.Split(' ', StringSplitOptions.RemoveEmptyEntries) ?? new string[0];
-                foreach (var scope in scopes)
-                {
-                  options.Scope.Add(scope);
-                }
-                
-                options.ClientId = settings.ClientId;
-                options.ClientSecret = settings.ClientSecret;
-                options.SaveTokens = true;
-            });
+          options.Scope.Add(scope);
         }
+
+        options.ClientId = settings.ClientId;
+        options.ClientSecret = settings.ClientSecret;
+        options.SaveTokens = true;
+
+        // note: here is some information how to introduce new claims
+        // https://leastprivilege.com/2017/11/15/missing-claims-in-the-asp-net-core-2-openid-connect-handler/
+      });
     }
+  }
 }
